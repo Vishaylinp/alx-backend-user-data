abc501ae8929e235c0d736b026d52a7fa9b8c0d4
Vishaylinp <vishaylinparshotam@gmail.com>
2024-09-03 13:32:11 +0000

    user object

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,6 +3,7 @@
 """
 from api.v1.auth.auth import Auth
 import base64
+from typing import TypeVar
 
 
 class BasicAuth(Auth):
@@ -42,8 +43,8 @@ class BasicAuth(Auth):
         except Exception:
             return None
 
-    def extract_user_credentials(self,
-                                 decoded_base64_authorization_header: str) -> (str, str):
+    def extract_user_credentials(
+            self, decoded_base64_authorization_header: str) -> (str, str):
         """user credentials"""
 
         if decoded_base64_authorization_header is None:
@@ -57,3 +58,24 @@ class BasicAuth(Auth):
 
         email, user_password = decoded_base64_authorization_header.split(":")
         return (email, user_password)
+
+    def user_object_from_credentials(
+            self, user_email: str, user_pwd: str) -> TypeVar('User'):
+        """user object from credentials"""
+
+        if user_email is None or not isinstance(user_email, str):
+            return None
+
+        if user_pwd is None or not isinstance(user_pwd, str):
+            return None
+
+        try:
+            user_search = User.search({"email": user_email})
+            if not user_search or user_search == []:
+                return None
+            for user in user_search:
+                if user.is_valid_password(user_pwd):
+                    return user
+            return None
+        except Exception:
+            return None
