ec5089371dc1f9b476d523bcdf876858c680385c
Vishaylinp <vishaylinparshotam@gmail.com>
2024-09-03 10:57:07 +0000

    base64

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,9 +2,25 @@
 """Basic Auth
 """
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
     """class that inherits from Auth
     """
-    pass
+
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """"extract authorization header"""
+
+        if authorization_header is None:
+            return None
+
+        if not isinstance(authorization_header, str):
+            return None
+
+        if not authorization_header.startswith("Basic "):
+            return None
+
+        hold = authorization_header.split(" ")[-1]
+        return hold
