6b5586658b7fbddc7fdfccf07345a779b4ebccc8
Vishaylinp <vishaylinparshotam@gmail.com>
2024-09-03 12:42:44 +0000

    basic

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -41,3 +41,19 @@ class BasicAuth(Auth):
             return decodes.decode("utf-8")
         except Exception:
             return None
+
+    def extract_user_credentials(self,
+                                 decoded_base64_authorization_header: str) -> (str, str):
+        """user credentials"""
+
+        if decoded_base64_authorization_header is None:
+            return (None, None)
+
+        if not isinstance(decoded_base64_authorization_header, str):
+            return (None, None)
+
+        if ":" not in decoded_base64_authorization_header:
+            return (None, None)
+
+        email, user_password = decoded_base64_authorization_header.split(":")
+        return (email, user_password)
