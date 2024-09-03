3e6e5f2a9319a5d87f518dda780862dc7b79c0ed
Vishaylinp <vishaylinparshotam@gmail.com>
2024-09-03 12:24:39 +0000

    base64 decode

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -24,3 +24,20 @@ class BasicAuth(Auth):
 
         hold = authorization_header.split(" ")[-1]
         return hold
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str:
+        """decode base64"""
+
+        if base64_authorization_header is None:
+            return None
+
+        if not isinstance(base64_authorization_header, str):
+            return None
+
+        try:
+            convert_to_bytes = base64_authorization_header.encode("utf-8")
+            decodes = base64.b64decode(convert_to_bytes)
+            return decodes.decode("utf-8")
+        except Exception:
+            return None
