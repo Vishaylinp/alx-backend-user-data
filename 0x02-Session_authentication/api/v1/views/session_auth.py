c27d7eeb3f1668c341ea844845bacaf7a9698683
Vishaylinp <vishaylinparshotam@gmail.com>
2024-09-05 09:46:52 +0000

    pycode error

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -36,7 +36,9 @@ def auth_session():
 
         return jsonify({"error": "wrong password"}), 401
 
-@app_views.route("/auth_session/logout", methods=["DELETE"], strict_slashes=False)
+
+@app_views.route("/auth_session/logout",
+                 methods=["DELETE"], strict_slashes=False)
 def delete_session():
     """logout"""
     from api.v1.app import auth
