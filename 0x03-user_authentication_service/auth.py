2eee3d4746ee0c8f1515c5353b89bc66927b3c7a
Vishaylinp <vishaylinparshotam@gmail.com>
2024-09-10 12:12:25 +0000

    find user by session

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -45,25 +45,39 @@ class Auth:
         """validate login"""
 
         try:
-            usr = self._db.find_user_by(email=email)
+            user = self._db.find_user_by(email=email)
 
         except NoResultFound:
             return False
 
         else:
             return bcrypt.checkpw(password.encode('utf-8'),
-                                  usr.hashed_password)
+                                  user.hashed_password)
 
     def create_session(self, email: str) -> str:
         """Create session
         """
 
         try:
-            usr = self._db.find_user_by(email=email)
+            user = self._db.find_user_by(email=email)
 
         except NoResultFound:
             return None
 
         sess_id = _generate_uuid()
-        self._db.update_user(usr.id, session_id=sess_id)
+        self._db.update_user(user.id, session_id=sess_id)
         return sess_id
+
+    def get_user_from_session_id(self, session_id: str) -> User:
+        """get user"""
+
+        if session_id is None:
+            return None
+
+        try:
+            user = self._db.find_user_by(session_id=session_id)
+
+        except NoResultFound:
+            return None
+
+        return user
