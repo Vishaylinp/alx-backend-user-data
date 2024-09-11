60eb32ffca2133f314c82cc75124eedbe895a83f
Vishaylinp <vishaylinparshotam@gmail.com>
2024-09-11 15:23:32 +0000

    error fix

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -108,9 +108,9 @@ class Auth:
 
         try:
             user = self._db.find_user_by(reset_token=reset_token)
-            n_password = self._hashed_password(password)
+            n_password = _hash_password(password)
             self._db.update_user(user.id, hashed_password=n_password,
-                                 reset_token=None)            
+                                 reset_token=None)
 
         except NoResultFound:
-            raise ValueError()
+            raise ValueError
