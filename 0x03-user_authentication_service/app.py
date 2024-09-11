dd6b3ef4b052d73dfa824f83df3fb4b4e85ea5e5
Vishaylinp <vishaylinparshotam@gmail.com>
2024-09-11 10:38:37 +0000

    add 200 http

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -73,7 +73,7 @@ def profile() -> str:
     user = AUTH.get_user_from_session_id(session_id)
     if user is None:
         abort(403)
-    return jsonify({"email": "<user email>"})
+    return jsonify({"email": "<user email>"}), 200
 
 
 if __name__ == "__main__":
