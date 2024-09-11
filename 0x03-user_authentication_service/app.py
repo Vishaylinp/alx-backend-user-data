532182c45d8184c2699bb73929606f67dacdcb2d
Vishaylinp <vishaylinparshotam@gmail.com>
2024-09-11 10:13:28 +0000

    fix error

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -32,7 +32,7 @@ def users() -> str:
         return jsonify({"message": "email already registered"}), 400
 
 
-@app.route("/session", methods=['POST'], strict_slashes=False)
+@app.route("/sessions", methods=['POST'], strict_slashes=False)
 def login() -> str:
     """login in
     """
@@ -43,7 +43,7 @@ def login() -> str:
     if AUTH.valid_login(email, password):
         session_id = AUTH.create_session(email)
         res = jsonify({"email": email, "message": "logged in"})
-        res.set_cookie("session_id", sess_id)
+        res.set_cookie("session_id", session_id)
         return res
     else:
         abort(401)
